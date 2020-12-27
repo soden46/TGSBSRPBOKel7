66231449a03b0d0b175f8ea8de19de098fc06fec
syarif soden <65828433+soden46@users.noreply.github.com>
2020-12-28 00:23:00 +0700

    Update Pelanggan.java

--- src/Data/Pelanggan.java
+++ src/Data/Pelanggan.java
@@ -1,49 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Data;
-/**
- *
- * @author SYARIF
- */
-public class Pelanggan{
-  
-    Integer IdPel;
-    protected String Nama;
-    protected String Alamat;
-    Double Harga;
 
-//    public Pelanggan() {
-//    }
+import Interface.PLN;
+import java.util.Scanner;
 
-    public Integer getIdPel(){
-        return IdPel;
-    }
-    public String getNama(){
-        return Nama;
-    }
-    public String getAlamat(){
-        return Alamat;
-    }
-
-    public void setIdPel(Integer IdPel){
-        this.IdPel=IdPel;
-    }
-    public void setNama(String Nama){
-        this.Nama=Nama;
-    }
-    public void setAlamat(String Alamat){
-        this.Alamat=Alamat;
-    }
+public abstract class Pelanggan implements PLN{
+    public String Nama;
+    public String IdPel;
+    public String Alamat;
+    public abstract void tampilData();
     
-    public void tampildata(){
-        System.out.println("Memeriksa Tagihan");
-        
-
-    }
-
-
-
 }
